0383d23552b89d8e683426ff3f88c049b7813045
Di <azazarova99@mail.ru>
2020-03-12 14:26:10 +0300

    fix: add filter-function

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,12 +4,10 @@ module.exports = function createDreamTeam(arr) {
     return false;
   }
   
+  arr = arr.filter(x => typeof x == "string");
+
   for(let i = 0; i < arr.length; i++) {
-    if(typeof arr[i] == "string" ) {
-      arr[i] = arr[i].trim().substring(0,1).toUpperCase();
-    } else {
-        return false;
-    }
+    arr[i] = arr[i].trim().substring(0,1).toUpperCase();
   }
 
   return result = arr.sort().join("");
