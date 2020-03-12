38c191d092a20f82ea09605b808bbc676df3cc18
Di <azazarova99@mail.ru>
2020-03-12 14:09:37 +0300

    feat: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  console.log(arr);
+  if(!Array.isArray(arr) || arr.length == 0){
+    return false;
+  }
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] == "string" ) {
+      arr[i] = arr[i].trim().substring(0,1).toUpperCase();
+    } else {
+        return false;
+    }
+  }
+
+  return result = arr.sort().join("");
+
 };
